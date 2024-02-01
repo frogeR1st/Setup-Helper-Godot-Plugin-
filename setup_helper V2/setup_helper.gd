651d31c49601fc5e760c3590d9eb4105ab969598
frogeR <lesnerblazej2@gmail.com>
2024-02-01 18:54:28 +0000

    Version 2 the cool one
    just added one more feature :(

---
+++ setup_helper V2/setup_helper.gd
@@ -0,0 +1,16 @@
+@tool
+extends EditorPlugin
+
+var setup_dock_1 = preload("res://addons/setup_helper/Window_Menu.tscn").instantiate()
+var setup_dock_2 = preload("res://addons/setup_helper/file_menu.tscn").instantiate()
+var setup_dock_3 = preload("res://addons/setup_helper/debug_menu.tscn").instantiate()
+
+func _enter_tree():
+	add_control_to_dock(DOCK_SLOT_RIGHT_BL, setup_dock_1)
+	add_control_to_dock(DOCK_SLOT_RIGHT_BL, setup_dock_2)
+	add_control_to_dock(DOCK_SLOT_RIGHT_BL, setup_dock_3)
+	
+func _exit_tree():
+	remove_control_from_docks(setup_dock_1)
+	remove_control_from_docks(setup_dock_2)
+	remove_control_from_docks(setup_dock_3)
